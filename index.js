2735d1246c8a2c4d9c3330723fa0da866ba4a274
Neal Patel <xnealx66@gmail.com>
2022-06-30 20:27:54 -0400

    commit tree 86345d20163e922d7136b4101ec5e07b793bb39a

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let multiply;
+let num1 = 2;
+let num2 = 31;
+multiply = num1 * num2;
 
+let random = Math.ceil(Math.random() * 10) + 6;
+
+let mod;
+let num3 = 10;
+let num4 = 6;
+mod = num3 % num4;
+
+let max =Math.max(10 , 20);
 // Write your code here
